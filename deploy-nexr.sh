0654c16c918936c0cd731f8812ebfb2e85fac4cd
halo.kim <4567493+4chicat@users.noreply.github.com>
2024-06-14 10:32:55 +0900

    Revert "[NEP-964] seatunnel-web 1.0.0-NR 지원 (#1)"
    This reverts commit 436bea8bc49f26dfe6b9b127394ed219915fa197.

--- deploy-nexr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mvn clean deploy -pl :seatunnel-datasource-client -am -DskipTests
-mvn clean deploy -pl :datasource-s3 -am -DskipTests
-mvn clean deploy -pl :datasource-jdbc-clickhouse -am -DskipTests
-mvn clean deploy -pl :datasource-jdbc-mysql -am -DskipTests
-mvn clean deploy -pl :datasource-jdbc-oracle -am -DskipTests
-mvn clean deploy -pl :datasource-jdbc-postgresql -am -DskipTests
-mvn clean deploy -pl :datasource-jdbc-sqlserver -am -DskipTests
-mvn clean deploy -pl :datasource-kafka -am -DskipTests
